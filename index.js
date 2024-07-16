be76163bd3f2ec9a10a4bf10ae331e6f61f89a2d
Abhishek-2400 <abhi2003shukla@gmail.com>
2024-07-16 20:47:14 +0530

    Applied Caching in nodejs  server

--- index.js
+++ index.js
@@ -1,24 +1,32 @@
-import express from "express"
+import express from "express";
+import client from './client.js';  // Redis client to interact with Redis
+
 const app = express();
 const PORT = 3000;
+
 app.get("/", async (req, res) => {
-    // 794 ms to bring the response
-    await fetch('https://jsonplaceholder.typicode.com/posts').
-        then((response) => {
-            if (!response.ok) {
-                throw new Error
-            }
-            return response.json()
-        }).then((data) => {
-            res.json({ data })
-        }).catch((err) => {
-            console.log(err)
-        })
+    try {
+        const cachevalue = await client.get('cachevalue');
+        if (cachevalue) {
+            return res.json(JSON.parse(cachevalue)); // Parse the cached string back to JSON
+        }
 
-    console.log(response)
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
 
-})
+        await client.set('cachevalue', JSON.stringify(data)); // Stringify the JSON before storing
+        await client.expire('cachevalue', 60);
+
+        res.json({ data });
+    } catch (err) {
+        console.log(err);
+        res.status(500).send('Internal Server Error');
+    }
+});
 
 app.listen(PORT, () => {
-    console.log(`Server is listening at ${3000}`)
-})
+    console.log(`Server is listening at ${PORT}`);
+});
